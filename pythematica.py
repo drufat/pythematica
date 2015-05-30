a8c274668ae652bcdfd4c8c17c30cd4c7bfb3a05
Dzhelil Rufat <drufat@caltech.edu>
2015-05-30 04:05:18 -0700

    Initial commit.

---
+++ pythematica.py
@@ -0,0 +1,140 @@
+import pexpect
+import sympy
+import re
+from sympy.printing import mathematica_code
+
+trans = {
+    'Exp'       : sympy.exp,
+    'Log'       : sympy.log,
+    'Sin'       : sympy.sin,
+    'Cos'       : sympy.cos,
+    'Tan'       : sympy.tan,
+    'Cot'       : sympy.cot,
+    'ArcSin'    : sympy.asin,
+    'ArcCos'    : sympy.acos,
+    'ArcTan'    : sympy.atan,
+    'ArcSinh'   : sympy.asinh,
+    'ArcCosh'   : sympy.acosh,
+    'ArcTanh'   : sympy.atanh,
+    'ArcCoth'   : sympy.acoth,
+    #'ArcSech'   : sympy.asech,
+    'Cosh'      : sympy.cosh,
+    'Coth'      : sympy.coth,
+    #'Csch'      : sympy.csch,
+    #'Sech'      : sympy.sech,
+    'Sinh'      : sympy.sinh,
+    'Tanh'      : sympy.tanh,
+    
+    'Infinity'  : sympy.oo,
+    'Plus'      : sympy.Add,
+    'Times'     : sympy.Mul,
+    'Power'     : sympy.Pow,
+    
+    'D'         : sympy.diff,
+    'Pi'        : sympy.pi,
+}
+
+def sanitize(mexpr):
+    '''
+    >>> sanitize('Hold[x]')
+    'x'
+    >>> sanitize('x')
+    'x'
+    '''
+    s = re.search('Hold\[(.*)\]', mexpr)
+    if s:
+        return s.group(1)
+    else:
+        return mexpr
+
+prompt = b'In\[\d+\]\:\='
+output = b'Out\[\d+\]//FullForm=(.*)\r\n\r\n'
+
+def to_mathematica(mexpr):
+    return sanitize(mathematica_code(mexpr))
+
+def from_mathematica(mexpr):
+    '''
+    >>> from sympy import *
+    >>> from sympy.abc import *
+    >>> F = from_mathematica
+    >>> assert F('Rational[1, 2]') == Integer(1)/Integer(2)
+
+#     >>> assert F('Times[Rational[1, 2], Power[x, 2]]') == 1/2*x**2
+#     >>> assert F('Power[E, x]') == exp(x)    
+#     >>> assert F('Power[Pi, Rational[1, 2]]') == sqrt(pi)
+#     >>> assert F('Plus[x, y]') == x + y
+#     >>> assert F('Plus[x, Times[-1, y]]') == x - y
+#     >>> assert F('Times[x, Power[y, -1]]') == x/ y
+#     >>> assert F('Cos[x]') == cos(x)
+    '''
+    mexpr = re.sub('\[', '(', mexpr)
+    mexpr = re.sub('\]', ')', mexpr)
+    return sympy.sympify(mexpr, locals=trans)
+
+class Mathematica(object):
+        
+    def __init__(self, program='wolfram'):
+        self.m = pexpect.spawn('{} {}'.format(program, '-rawterm'))
+        self.m.expect(prompt)
+
+    def call_string(self, strexpr):
+        self.m.sendline('FullForm[{}]'.format(strexpr).encode())
+        self.m.expect(prompt)
+        s = re.search(output, self.m.before, flags=re.DOTALL)
+        rslt = s.group(1).strip().decode()
+        rslt = re.sub('>', '', rslt)
+        rslt = re.sub('\s+', ' ', rslt) #replace white spaces
+        rslt = rslt.strip()
+        return rslt
+
+    def __call__(self, mexpr):
+        if type(mexpr) is str:
+            return self.call_string(mexpr)
+        else: #assume sypmpy expression
+            mexpr = to_mathematica(mexpr)
+            mexpr = self.call_string(mexpr)
+            mexpr = from_mathematica(mexpr)
+            return mexpr
+
+    def __del__(self):
+        self.m.sendline(b'Quit')
+        self.m.expect(pexpect.EOF)
+
+def example1():
+    
+    print('\nMathematica:')
+    m = Mathematica()
+    print(m( 'Integrate[x, x]' ))
+    print(m( 'Integrate[Exp[x], x]' ))
+    print(m( 'Integrate[Exp[-x^2], {x, -Infinity, +Infinity}]' ))
+    print(m( 'x+y' ))
+    print(m( 'x-y' ))
+    print(m( 'x/y' ))
+    print(m( 'D[Sin[x], x]' ))
+    print(m( 'D[Sin[Sin[x]], x]' ))
+    print(m( 'Integrate[x^2, {x, -1, 1}]' ))
+    print(m( 'FourierTransform[Sin[x], {x, y}, {u, v}]' ))
+    print(m( 'InverseFourierTransform[Sin[x], {x, y}, {u, v}]' ))
+
+def example2():
+
+    from sympy import Integral, exp, diff, sin, oo
+    from sympy.abc import x, y
+
+    print('\nSympy:')
+    m = Mathematica()
+    print(m( Integral(x, x) ))
+    print(m( Integral(exp(x), x) ))
+    print(m( Integral(exp(-x**2), (x, -oo, +oo)) ))
+    print(m( x+y ))
+    print(m( x-y ))
+    print(m( x/y ))
+    print(m( diff(sin(x), x) ))
+    print(m( diff(sin(sin(x)), x) ))
+    print(m( Integral(x**2, (x, -1, +1)) ))
+
+if __name__ == '__main__':
+    example1()
+    example2()
+    
