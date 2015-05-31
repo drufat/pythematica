75593ab229d194afe4cdaf7130e838048bb4377f
Dzhelil Rufat <drufat@caltech.edu>
2015-05-31 14:37:14 -0700

    Add doctests and examples.

--- README.rst
+++ README.rst
@@ -1,1 +1,69 @@
-Calling Mathematica with SymPy expressions.
+Pythematica
+=============
+
+Pythematica is python package that allows for two way communication between Python 
+and Mathematica. On the python side it expresses python formulae as sympy expressoins. 
+It translates those into strings parsable by Mathematica, evaluates them in the 
+Mathematica interpreter, and then parses the output back into sympy expressions. 
+
+Examples:
+----------
+
+:: 
+
+	>>> from pythematica import Pythematica
+	
+    >>> from sympy import exp, sin, cos, oo, sqrt, pi, I
+    >>> from sympy.abc import x, y, u, v, n, k
+
+    >>> m = Pythematica()
+    >>> m.Integrate(x, x) 
+    x**2/2
+    >>> m.Integrate(exp(x), x) 
+    exp(x)
+    >>> m.Integrate(exp(-x**2), (x, -oo, +oo))
+    sqrt(pi)
+    >>> m( x+y )
+    x + y
+    >>> m( x-y )
+    x - y
+    >>> m( x/y )
+    x/y
+    >>> m.Sum(1/n**k, (k, 0, oo))
+    n/(n - 1)
+    >>> m.Sum(1/n**k, (k, 1, oo))
+    1/(n - 1)
+    >>> m.D(sin(x), x)
+    cos(x)
+    >>> m.D(sin(sin(x)), x)
+    cos(x)*cos(sin(x))
+    >>> m.Integrate(x**2, (x, -1, +1))
+    2/3
+    >>> m.Integrate(1/(x**2 + 1), (x, 0, +oo))
+    pi/2
+    >>> m.Integrate(sqrt(1 - x**2), (x, 0, 1))
+    pi/4
+    >>> m.Integrate(sin(x)*cos(x), (x, 0, pi))
+    0
+    >>> m.Integrate(sin(x)*cos(2*x), (x, 0, pi))
+    -2/3
+    >>> m.FourierTransform(exp(I*x), (x, y), (u, v))
+    2*pi*DiracDelta(v)*DiracDelta(u + 1)
+    >>> m.InverseFourierTransform(exp(I*x), (x, y), (u, v))
+    2*pi*DiracDelta(v)*DiracDelta(u - 1)
+    >>> m.InverseFourierTransform(
+    ...     m.FourierTransform(exp(I*x), (x, y), (u, v)), 
+    ...     (u, v), (x, y))
+    exp(I*x)
+    >>> m.Integrate(exp(I*x), (x, 0, pi))
+    2*I
+
+    
+Installation:
+--------------
+
+To install simply run:
+
+::
+	>> python setup.py install
+	
--- pythematica.py
+++ pythematica.py
@@ -92,6 +92,21 @@ def from_mathematica(mexpr):
     return sympy.sympify(mexpr, locals=trans)
 
 class Mathematica(object):
+    '''
+    >>> m = Mathematica()
+    >>> m( 'Integrate[x, x]' )
+    'Times[Rational[1, 2], Power[x, 2]]'
+    >>> m( 'x+y' )
+    'Plus[x, y]'
+    >>> m( 'x-y' )
+    'Plus[x, Times[-1, y]]'
+    >>> m( 'x/y' )
+    'Times[x, Power[y, -1]]'
+    >>> m( 'D[Sin[x], x]' )
+    'Cos[x]'
+    >>> m( 'Integrate[x^2, {x, -1, 1}]' )
+    'Rational[2, 3]'
+    '''
         
     def __init__(self, program='wolfram'):
         self.m = pexpect.spawn('{} {}'.format(program, '-rawterm'))
@@ -120,6 +135,52 @@ def get_mathematica_func(m, name):
     return func
 
 class Pythematica(object):
+    '''
+    >>> from sympy import exp, sin, cos, oo, sqrt, pi, I
+    >>> from sympy.abc import x, y, u, v, n, k
+
+    >>> m = Pythematica()
+    >>> m.Integrate(x, x) 
+    x**2/2
+    >>> m.Integrate(exp(x), x) 
+    exp(x)
+    >>> m.Integrate(exp(-x**2), (x, -oo, +oo))
+    sqrt(pi)
+    >>> m( x+y )
+    x + y
+    >>> m( x-y )
+    x - y
+    >>> m( x/y )
+    x/y
+    >>> m.Sum(1/n**k, (k, 0, oo))
+    n/(n - 1)
+    >>> m.Sum(1/n**k, (k, 1, oo))
+    1/(n - 1)
+    >>> m.D(sin(x), x)
+    cos(x)
+    >>> m.D(sin(sin(x)), x)
+    cos(x)*cos(sin(x))
+    >>> m.Integrate(x**2, (x, -1, +1))
+    2/3
+    >>> m.Integrate(1/(x**2 + 1), (x, 0, +oo))
+    pi/2
+    >>> m.Integrate(sqrt(1 - x**2), (x, 0, 1))
+    pi/4
+    >>> m.Integrate(sin(x)*cos(x), (x, 0, pi))
+    0
+    >>> m.Integrate(sin(x)*cos(2*x), (x, 0, pi))
+    -2/3
+    >>> m.FourierTransform(exp(I*x), (x, y), (u, v))
+    2*pi*DiracDelta(v)*DiracDelta(u + 1)
+    >>> m.InverseFourierTransform(exp(I*x), (x, y), (u, v))
+    2*pi*DiracDelta(v)*DiracDelta(u - 1)
+    >>> m.InverseFourierTransform(
+    ...     m.FourierTransform(exp(I*x), (x, y), (u, v)), 
+    ...     (u, v), (x, y))
+    exp(I*x)
+    >>> m.Integrate(exp(I*x), (x, 0, pi))
+    2*I
+    '''
 
     def __init__(self, program='wolfram'):
         self.m = Mathematica(program=program)
@@ -132,56 +193,4 @@ class Pythematica(object):
     
     def __getattr__(self, name):
         return get_mathematica_func(self.m, name)
-    
-def example1():
-    
-    print('\nMathematica:')
-    m = Mathematica()
-    print(m( 'Integrate[x, x]' ))
-    print(m( 'Integrate[Exp[x], x]' ))
-    print(m( 'Integrate[Exp[-x^2], {x, -Infinity, +Infinity}]' ))
-    print(m( 'x+y' ))
-    print(m( 'x-y' ))
-    print(m( 'x/y' ))
-    print(m( 'D[Sin[x], x]' ))
-    print(m( 'D[Sin[Sin[x]], x]' ))
-    print(m( 'Integrate[x^2, {x, -1, 1}]' ))
-    print(m( 'FourierTransform[Exp[I x], {x, y}, {u, v}]' ))
-    print(m( 'InverseFourierTransform[Exp[I x], {x, y}, {u, v}]' ))
-    print(m( 'Integrate[Exp[I x], {x, 0, Pi}]' ))
-    print(m( 'I*I' ))
-
-def example2():
-
-    from sympy import exp, sin, cos, oo, sqrt, pi, I
-    from sympy.abc import x, y, u, v, n, k
-
-    print('\nPythematica:')
-    m = Pythematica()
-    print(m.Integrate(x, x))
-    print(m.Integrate(exp(x), x))
-    print(m.Integrate(exp(-x**2), (x, -oo, +oo)))
-    print(m( x+y ))
-    print(m( x-y ))
-    print(m( x/y ))
-    print(m.Sum(1/n**k, (k, 0, oo)))
-    print(m.Sum(1/n**k, (k, 1, oo)))
-    print(m.D(sin(x), x))
-    print(m.D(sin(sin(x)), x))
-    print(m.Integrate(x**2, (x, -1, +1)))
-    print(m.Integrate(1/(x**2 + 1), (x, 0, +oo)))
-    print(m.Integrate(sqrt(1 - x**2), (x, 0, 1)))
-    print(m.Integrate(sin(x)*cos(x), (x, 0, pi)))
-    print(m.Integrate(sin(x)*cos(2*x), (x, 0, pi)))
-    print(m.FourierTransform(exp(I*x), (x, y), (u, v)))
-    print(m.InverseFourierTransform(exp(I*x), (x, y), (u, v)))
-    print(m.InverseFourierTransform(
-            m.FourierTransform(exp(I*x), (x, y), (u, v)), 
-            (u, v), (x, y)))
-    print(m.Integrate(exp(I*x), (x, 0, pi)))
-    print(m( I*I ))
-
-if __name__ == '__main__':
-    example1()
-    example2()
-    
+        
